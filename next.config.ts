f6b8f69b416f1676436da756e5feda7d2196114d
Prish20 <prish@students.uonbi.ac.ke>
2024-11-18 14:36:48 +0300

    Implement fetching post details from sanity using nextjs ppr startegy

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-      dangerouslyAllowSVG: true,
-      remotePatterns: [
-          {
-              protocol: "https",
-              hostname: "*"
-          }
-      ],
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+    ],
+  },
+  experimental: {
+    ppr: "incremental",
+  },
+  devIndicators: {
+    appIsrStatus: true,
+    buildActivity: true,
+    buildActivityPosition: "bottom-right",
   },
 };
 
